2a998c8fca41701f25a1fae7300d6b833b38ef85
IvoSilvestre <60756042+IvoSilvestre@users.noreply.github.com>
2020-11-21 12:17:32 -0300

    Update residuals.SLRMss.R

--- R/residuals.SLRMss.R
+++ R/residuals.SLRMss.R
@@ -2,7 +2,7 @@ residuals.SLRMss <-
 function(fit,H0=FALSE,std=FALSE){
    res=(fit$y-fitted(fit,H0=H0))
    if(std==T){
-   return(res/coef(fit,H0=H0))  
+   return(res/coef(fit,H0=H0)$phi)  
    }else{
    return(res)  
    }
