b8e3f3479b2c70210ce0c51bcc875272ebc12ad5
IvoSilvestre <60756042+IvoSilvestre@users.noreply.github.com>
2021-06-16 17:03:48 -0300

    Create plot.SLRMss.R

---
+++ R/plot.SLRMss.R
@@ -0,0 +1,16 @@
+plot.SLRMss=
+  function(x){
+    par(mfrow=c(2,2))
+    resid=residuals(x,std=T)
+    plot(fitted(x),resid,
+         xlab="Fitted Values",ylab="Standardized Residuals",
+         main="Against Fitted Values",pch=16)
+    plot(resid,xlab="Index",
+         ylab="Standardized Residuals",main="Against Index",pch=16)
+    qqnorm(resid,main="Normal Q-Q Plot",pch=16)
+    abline(0,1,col="red")
+    plot(density(resid),xlab="Standardized Residuals",
+         ylab="Density",main="Density Estimate")
+    points(resid,rep(0,length(resid)),pch=16)
+    par(mfrow=c(1,1))
+  }
