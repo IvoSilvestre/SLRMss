c85215dace286a8058f9b0cfee524b31bd54bb2e
IvoSilvestre <60756042+IvoSilvestre@users.noreply.github.com>
2021-07-07 19:08:16 -0300

    Update envplot.R

--- R/envplot.R
+++ R/envplot.R
@@ -55,9 +55,9 @@ function (object, J=100, conf = 0.95, seed = NULL, H0=FALSE,colors=c("red","gree
     if(missingArg(ylab)) ylab="Sample Quantiles"
     if(missingArg(main)) main=paste0("Envelope plot - ",100*conf,"% confidence")
     ylim=c(min(faixay,min(rqobs)),max(faixay,max(rqobs)))
-    if(length(colors)>=2){cores=ifelse(sort(rqobs)<infsup[1,] | sort(rqobs) > infsup[2,],colors[1],colors[2])}
+    if(length(colors)>=2){colors=ifelse(sort(rqobs)<infsup[1,] | sort(rqobs) > infsup[2,],colors[1],colors[2])}
     qq0 <- qqnorm(rqobs, main = main, xlab = xlab,ylab=ylab, 
-        pch = 1,col=cores, ylim = faixay)
+        pch = 1,col=colors, ylim = faixay)
     eixox <- sort(qq0$x)
     #for (i in 1:length(qq0$x)) {
     #    if (sort(qq0$y)[i] < infsup[1, i] | sort(qq0$y)[i] > 
