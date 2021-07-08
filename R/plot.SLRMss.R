1765e959d09208a881b4051905ffda37c2a22cc1
IvoSilvestre <60756042+IvoSilvestre@users.noreply.github.com>
2021-07-08 17:21:55 -0300

    Update plot.SLRMss.R

--- R/plot.SLRMss.R
+++ R/plot.SLRMss.R
@@ -1,16 +1,18 @@
 plot.SLRMss<-
-  function(x){
+  function(x,xlab=c("Fitted Values","Index","Theoretical Quantiles","Standardized Residuals"),
+             ylab=c("Standardized Residuals", "Standardized Residuals","Sample Quantiles","Density"),
+             main=c("Against Fitted Values","Against Index","Normal Q-Q Plot","Density Estimate")){
     par(mfrow=c(2,2))
     resid=residuals(x,std=T)
     plot(fitted(x),resid,
-         xlab="Fitted Values",ylab="Standardized Residuals",
-         main="Against Fitted Values",pch=16)
-    plot(resid,xlab="Index",
-         ylab="Standardized Residuals",main="Against Index",pch=16)
-    qqnorm(resid,main="Normal Q-Q Plot",pch=16)
+         xlab=xlab[1],ylab=ylab[1],
+         main=main[1],pch=16)
+    plot(resid,xlab=xlab[2],
+         ylab=ylab[2],main=main[2],pch=16)
+    qqnorm(resid,xlab=xlab[3],ylab=ylab[3],main=main[3],pch=16)
     abline(0,1,col="red")
-    plot(density(resid),xlab="Standardized Residuals",
-         ylab="Density",main="Density Estimate")
+    plot(density(resid),xlab=xlab[4],
+         ylab=ylab[4],main=main[4])
     points(resid,rep(0,length(resid)),pch=16)
     par(mfrow=c(1,1))
   }
