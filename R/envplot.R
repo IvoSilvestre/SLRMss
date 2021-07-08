18e65a0a8badb8225d297ce7e60993fba0d52a74
IvoSilvestre <60756042+IvoSilvestre@users.noreply.github.com>
2021-07-08 16:50:36 -0300

    Update envplot.R

--- R/envplot.R
+++ R/envplot.R
@@ -55,7 +55,14 @@ function (object, J=100, conf = 0.95, seed = NULL, H0=FALSE,colors=c("red","gree
     if(missingArg(ylab)) ylab="Sample Quantiles"
     if(missingArg(main)) main=paste0("Envelope plot - ",100*conf,"% confidence")
     ylim=c(min(faixay,min(rqobs)),max(faixay,max(rqobs)))
-    if(length(colors)>=2){colors=ifelse(sort(rqobs)<infsup[1,] | sort(rqobs) > infsup[2,],colors[1],colors[2])}
+    if(length(colors)>=2){
+        colors=ifelse(sort(rqobs)<infsup[1,] | sort(rqobs) > infsup[2,],colors[1],colors[2])
+        if(length(colors)>2) warning("Only the first two colors entries were used.")
+    }
+    if(length(pch)>=2){
+        pch=ifelse(sort(rqobs)<infsup[1,] | sort(rqobs) > infsup[2,],pch[1],pch[2])
+        if(length(pch)>2) warning("Only the first two pch entries were used.")
+    }
     qq0 <- qqnorm(sort(rqobs), main = main, xlab = xlab,ylab=ylab, 
         col=colors,pch=pch, ylim = faixay)
     eixox <- sort(qq0$x)
@@ -70,9 +77,11 @@ function (object, J=100, conf = 0.95, seed = NULL, H0=FALSE,colors=c("red","gree
     #            pch = 20)
     #    }
     #}
-    lines(eixox, media)
-    lines(eixox, infsup[1, ])
-    lines(eixox, infsup[2, ])
+    if(length(lty)>2)warning("Only the first two lty entries were used")
+    if(length(lty)==1) lty[2]=lty
+    lines(eixox, media,lty=lty[1])
+    lines(eixox, infsup[1, ],lty=lty[2])
+    lines(eixox, infsup[2, ],lty=lty[2])
     out=NULL
     out$x=qq0$x
     out$y=qq0$y
